1dc1e78ecc3f67f80e4823ba8de0d11b811b0c53
Andrew Peck <andrew.peck@cern.ch>
2022-12-09 16:34:38 -0500

    add pkg requirements

--- system-install.el
+++ system-install.el
@@ -1,8 +1,33 @@
-;;; -*- lexical-binding: t; -*-
+;;; system-install.el --- Wrappers for package managers-*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021-2022 Andrew Peck
+
+;; Author: Andrew Peck <andrew.peck@cern.ch>
+;; URL: https://github.com/andrewpeck/system-install.el
+;; Version: 0.0.0
+;; Package-Requires: ((s))
+;; Keywords: tools vhdl fpga
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+;;; Commentary:
 
 ;;; Code:
 
-;; package specific commands and flags
+(require 's)
 
 (defvar system-install-package-cache-file "~/.emacs.d/system-package-cache.json")
 (defvar system-install-package-cache-refresh-days 7)
