eb347b45991d62c7287fff6fc1a66b4167bd7a8b
Andrew Peck <andrew.peck@cern.ch>
2022-12-11 12:53:00 -0500

    make private functions private

--- system-install.el
+++ system-install.el
@@ -35,69 +35,69 @@
 (require 'cl-lib)
 (require 'ansi-color)
 
-(defvar system-install-package-cache-file
+(defvar system-install--package-cache-file
   (concat user-emacs-directory "system-package-cache.json"))
-(defvar system-install-package-cache-refresh-days 7)
+(defvar system-install--package-cache-refresh-days 7)
 
 ;; trim the 2 header lines off of the package list output, and remove duplicate lines
 (defvar system-install--dnf-filter-cmd
   "awk -F. 'NF > 2 {this = $1; if (this != prev) {print this}; prev = this}'")
 
-(defvar system-install-exe
+(defvar system-install--exe
   (cond ((executable-find "dnf")    'dnf)
         ((executable-find "pacman") 'pacman)
         ((executable-find "apt")    'apt)))
 
-(defun system-install-get-package-cmd ()
-  (pcase system-install-exe
+(defun system-install--get-package-cmd ()
+  (pcase system-install--exe
     ('dnf    "dnf")
     ('pacman "pacman")
     ('apt    "apt")))
 
-(defun system-install-get-package-info-flag ()
-  (pcase system-install-exe
+(defun system-install--get-package-info-flag ()
+  (pcase system-install--exe
     ('dnf    "info")
     ('pacman "-Si")
     ('apt    "show")))
 
-(defun system-install-get-package-install-flag ()
-  (pcase system-install-exe
+(defun system-install--get-package-install-flag ()
+  (pcase system-install--exe
     ('dnf    "install")
     ('pacman "-S")
     ('apt    "install")))
 
-(defun system-install-get-package-update-flag ()
-  (pcase system-install-exe
+(defun system-install--get-package-update-flag ()
+  (pcase system-install--exe
     ('dnf    "update")
     ('pacman "-Sy")
     ('apt    "install")))
 
-(defun system-install-get-package-remove-flag ()
-  (pcase system-install-exe
+(defun system-install--get-package-remove-flag ()
+  (pcase system-install--exe
     ('dnf    "remove")
     ('pacman "-R")
     ('apt    "uninstall")))
 
-(defun system-install-get-system-upgrade-flag ()
-  (pcase system-install-exe
+(defun system-install--get-system-upgrade-flag ()
+  (pcase system-install--exe
     ('dnf    "update")
     ('pacman "-Syu")
     ('apt    "upgrade")))
 
-(defun system-install-get-package-list-cmd ()
-  (pcase system-install-exe
+(defun system-install--get-package-list-cmd ()
+  (pcase system-install--exe
     ('dnf    (concat  "dnf -C list available | " system-install--dnf-filter-cmd))
     ('pacman "pacman -Sl | awk '{print $2}'")
     ('apt    "apt-cache search . | awk '{print $1}'")))
 
-(defun system-install-get-installed-package-list-cmd ()
-  (pcase system-install-exe
+(defun system-install--get-installed-package-list-cmd ()
+  (pcase system-install--exe
     ('dnf (concat  "dnf -C list installed | " system-install--dnf-filter-cmd))
     ('pacman "pacman -Q | awk '{print $2}'")
     ('apt "apt list --installed 2> /dev/null | awk -F\/ '/\[installed/ {print $1}'")))
 
-(defun system-install-get-clean-cache-cmd ()
-  (pcase system-install-exe
+(defun system-install--get-clean-cache-cmd ()
+  (pcase system-install--exe
     ('pacman "pacman -Sc")
     ('apt "apt-get clean")
     ('dnf "dnf clean all")))
@@ -106,92 +106,92 @@
 (defun system-install-clean-cache ()
   "Clean system package cache"
   (interactive)
-  (async-shell-command (system-install-get-clean-cache-cmd)))
+  (async-shell-command (system-install--get-clean-cache-cmd)))
 
 ;; generic functions
 
-(defun system-install-get-package-list ()
+(defun system-install--get-package-list ()
   ;; if we have no cache, or it is out of date generate one
-  (if (or  (not (file-exists-p system-install-package-cache-file))
+  (if (or  (not (file-exists-p system-install--package-cache-file))
            (> (time-to-seconds
                (time-subtract (current-time)
                               (file-attribute-modification-time
-                               (file-attributes system-install-package-cache-file ))))
-              (* 60 60 24 system-install-package-cache-refresh-days)))
+                               (file-attributes system-install--package-cache-file ))))
+              (* 60 60 24 system-install--package-cache-refresh-days)))
       (system-install-update)
-    (let ((package-list (s-split "\n" (shell-command-to-string (system-install-get-package-list-cmd)) t)))
-      (with-temp-file system-install-package-cache-file
+    (let ((package-list (s-split "\n" (shell-command-to-string (system-install--get-package-list-cmd)) t)))
+      (with-temp-file system-install--package-cache-file
         (insert (json-encode package-list)))
       package-list)
 
     ;; if it exists and is up to date, just return the cache
     (let ((json-array-type 'list))
-      (json-read-file system-install-package-cache-file))))
+      (json-read-file system-install--package-cache-file))))
 
-(defun system-install-get-installed-package-list ()
+(defun system-install--get-installed-package-list ()
   (s-split "\n"
            (shell-command-to-string
-            (system-install-get-installed-package-list-cmd)) t))
+            (system-install--get-installed-package-list-cmd)) t))
 
-(define-minor-mode system-install-run-minor-mode
+(define-minor-mode system-install--run-minor-mode
   "Minor mode for buffers running system install commands"
   :keymap '(("q" .  bury-buffer)))
 
-(cl-defun system-install-run (subcmd &key args noroot)
+(cl-defun system-install--run (subcmd &key args noroot)
   (let* ((name (format "%s" subcmd))
          (buf (format "*%s*" name)))
 
     (async-shell-command
      (concat
       (if noroot "" "sudo ")
-      (system-install-get-package-cmd) " "
+      (system-install--get-package-cmd) " "
       subcmd " "
       (when args (if (listp args) (string-join args " ") args)))
      buf)
 
     (with-current-buffer buf
       (ansi-color-apply-on-region (point-min) (point-max))
-      (system-install-run-minor-mode))))
+      (system-install--run-minor-mode))))
 
 ;;;###autoload
 (defun system-install (package)
   "Install `package' via system installer"
   (interactive
-   (list (completing-read "Formula: " (system-install-get-package-list) nil t)))
-  (system-install-run (system-install-get-package-install-flag) :args package))
+   (list (completing-read "Formula: " (system-install--get-package-list) nil t)))
+  (system-install--run (system-install--get-package-install-flag) :args package))
 
 ;;;###autoload
 (defun system-install-upgrade-package (package)
   "Upgrade `package' to the latest version"
   (interactive
-   (list (completing-read "Formula: " (system-install-get-installed-package-list) nil t)))
-  (system-install-run (system-install-get-package-update-flag) :args package))
+   (list (completing-read "Formula: " (system-install--get-installed-package-list) nil t)))
+  (system-install--run (system-install--get-package-update-flag) :args package))
 
 ;;;###autoload
 (defun system-install-remove-package (package)
   "Remove `package' using system package manager"
   (interactive
-   (list (completing-read "Formula: " (system-install-get-installed-package-list) nil t)))
-  (system-install-run (system-install-get-package-remove-flag) :args package))
+   (list (completing-read "Formula: " (system-install--get-installed-package-list) nil t)))
+  (system-install--run (system-install--get-package-remove-flag) :args package))
 
 ;;;###autoload
 (defun system-install-upgrade ()
   "Upgrade all system packages"
   (interactive)
-  (system-install-run (system-install-get-system-upgrade-flag)))
+  (system-install--run (system-install--get-system-upgrade-flag)))
 
 ;;;###autoload
 (defun system-install-update ()
   "Update the package database"
   (interactive)
-  (system-install-run (system-install-get-package-update-flag)))
+  (system-install--run (system-install--get-package-update-flag)))
 
 ;;;###autoload
 (defun system-install-package-info (package)
   "Display `info' output for `package'"
   (interactive
-   (list (completing-read "Formula: " (system-install-get-package-list) nil t)))
-  (system-install-run (system-install-get-package-info-flag) :args package :noroot t))
+   (list (completing-read "Formula: " (system-install--get-package-list) nil t)))
+  (system-install--run (system-install--get-package-info-flag) :args package :noroot t))
 
 (provide 'system-install)
 ;;; system-install.el ends here
